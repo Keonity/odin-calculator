63e6f0b2e7bf360d48014a8a581d827552720b2f
Keonity <56704285+Keonity@users.noreply.github.com>
2024-05-30 00:43:34 -0700

    Added basic calculator functionality.
    Still need to implement implicit equals functionality.

---
+++ script.js
@@ -0,0 +1,105 @@
+let numContainer = document.querySelector(".numButtons");
+let calcDisplay = document.querySelector(".calculation");
+
+let currNum = 0;
+let currOp = "";
+let secondNum = 0;
+let currMode = "first";
+
+function containsNumbers(str) {
+    let nums = "1234567890";
+    for (let i = 0; i < str.length; i++) {
+        if (!nums.includes(str.charAt(i))) {
+            return false;
+        }; 
+    }
+    return true;
+}
+
+function sum(a, b) {
+    calcDisplay.textContent = a + b;
+    currNum = parseInt(calcDisplay.textContent);
+    secondNum = 0;
+    return a + b;
+}
+
+function multiply(a, b) {
+    calcDisplay.textContent = a * b;
+    currNum = parseInt(calcDisplay.textContent);
+    secondNum = 0;
+    return a * b;
+}
+
+function minus(a, b) {
+    calcDisplay.textContent = a - b;
+    currNum = parseInt(calcDisplay.textContent);
+    secondNum = 0;
+    return a - b;
+}
+
+function divide(a, b) {
+    calcDisplay.textContent = a / b;
+    currNum = parseInt(calcDisplay.textContent);
+    secondNum = 0;
+    return a/b;
+}
+
+numContainer.addEventListener("click", function(e) {
+    console.log(e.target.id);
+
+    if (calcDisplay.textContent === "Calculation" || !containsNumbers(calcDisplay.textContent) || (currOp !== "" && secondNum === 0)) {
+        calcDisplay.textContent = e.target.id;
+    }
+    else {
+        calcDisplay.textContent += e.target.id;
+    }
+    
+    if (currOp === "") {
+        currNum = parseInt(calcDisplay.textContent);
+    }
+    else {
+        secondNum = parseInt(calcDisplay.textContent);
+    }
+    console.log("Curr Num: " + currNum);
+    console.log("Second Num: " + secondNum);
+});
+
+let opContainer = document.querySelector(".opButtons");
+
+opContainer.addEventListener("click", function(e) {
+    if (currMode !== "op") {
+        currMode == "op";
+    } 
+
+    // console.log(e.target.textContent);
+    if (e.target.textContent !== '=') {
+        calcDisplay.textContent = e.target.textContent;
+        currOp = calcDisplay.textContent;
+    }
+    console.log("Current Operator: " + currOp);
+
+    if (secondNum === 0 && (e.target.textContent !== currOp)) {
+        currOp = e.target.textContent;
+    }
+
+    if (secondNum !== 0) {
+        switch (currOp) {
+            case ('+'):
+                sum(currNum, secondNum);
+                break;
+            case ('-'):
+                minus(currNum, secondNum);
+                break;
+            case ('*'):
+                multiply(currNum, secondNum);
+                break;
+            case ('/'):
+                divide(currNum, secondNum);
+                break;
+            default:
+                currNum = parseInt(calcDisplay.textContent);
+                break;
+        }
+    }
+
+});
